9547ea228996aa7d0f089e1a56dab5f4c64a9208
FV-SU24a <felicia.vasquez@elev.ga.lbs.se>
2024-11-29 12:06:14 +0100

    rah

---
+++ Assets/Scripts/Movment.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movment : MonoBehaviour
+{
+    Rigidbody2D RiBo;
+    // Start is called before the first frame update
+    void Start()
+    {
+        RiBo = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RiBo.velocity = new Vector2(0, 0);
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            RiBo.velocity = new Vector2(2, 0);
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            RiBo.velocity = new Vector2(-2, 0);
+
+        }
+    }
+}
